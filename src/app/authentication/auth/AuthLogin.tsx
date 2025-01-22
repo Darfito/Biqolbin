c91f474a445f6a798c71c2f1171593de8005c974
Darfito <darfito.danurdoro-2021@fst.unair.ac.id>
2025-01-22 22:36:14 +0700

    feat: installment the auth component

--- src/app/authentication/auth/AuthLogin.tsx
+++ src/app/authentication/auth/AuthLogin.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+'use client';
+
+import React, { useState, FormEvent } from "react";
 import {
   Box,
   Typography,
@@ -11,6 +13,8 @@ import {
 import Link from "next/link";
 
 import CustomTextField from "@/app/(DashboardLayout)/components/forms/theme-elements/CustomTextField";
+import { login } from "../login/action";
+
 
 interface loginType {
   title?: string;
@@ -18,83 +22,113 @@ interface loginType {
   subtext?: JSX.Element | JSX.Element[];
 }
 
-const AuthLogin = ({ title, subtitle, subtext }: loginType) => (
-  <>
-    {title ? (
-      <Typography fontWeight="700" variant="h2" mb={1}>
-        {title}
-      </Typography>
-    ) : null}
+const AuthLogin = ({ title, subtitle, subtext }: loginType) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    {subtext}
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-    <Stack>
-      <Box>
-        <Typography
-          variant="subtitle1"
-          fontWeight={600}
-          component="label"
-          htmlFor="username"
-          mb="5px"
-        >
-          Username
+    const formData = new FormData();
+    formData.append("email", email);
+    formData.append("password", password);
+
+    await login(formData);
+  };
+
+  return (
+    <>
+      {title ? (
+        <Typography fontWeight="700" variant="h2" mb={1}>
+          {title}
         </Typography>
-        <CustomTextField variant="outlined" fullWidth />
-      </Box>
-      <Box mt="25px">
-        <Typography
-          variant="subtitle1"
-          fontWeight={600}
-          component="label"
-          htmlFor="password"
-          mb="5px"
-        >
-          Password
-        </Typography>
-        <CustomTextField type="password" variant="outlined" fullWidth />
-      </Box>
-      <Stack
-        justifyContent="space-between"
-        direction="row"
-        alignItems="center"
-        my={2}
-      >
-        <FormGroup>
-          <FormControlLabel
-            control={<Checkbox defaultChecked />}
-            label="Remeber this Device"
-          />
-        </FormGroup>
-        <Typography
-          component={Link}
-          href="/"
-          fontWeight="500"
-          sx={{
-            textDecoration: "none",
-            color: "primary.main",
-          }}
-        >
-          Forgot Password ?
-        </Typography>
-      </Stack>
-    </Stack>
-    <Box>
-      <Button
-        sx={{ 
-          color: "white",
-         }}
-        variant="contained"
-        size="large"
-        fullWidth
-        component={Link}
-        href="/"
-        type="submit"
-      >
-        Sign In
-      </Button>
-    </Box>
-    {subtitle}
-  </>
-);
+      ) : null}
+
+      {subtext}
+
+      <form onSubmit={handleSubmit}>
+        <Stack>
+          <Box>
+            <Typography
+              variant="subtitle1"
+              fontWeight={600}
+              component="label"
+              htmlFor="email"
+              mb="5px"
+            >
+              Email
+            </Typography>
+            <CustomTextField
+              id="email"
+              type="email"
+              variant="outlined"
+              fullWidth
+              value={email}
+              onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setEmail(e.target.value)}
+              required
+            />
+          </Box>
+          <Box mt="25px">
+            <Typography
+              variant="subtitle1"
+              fontWeight={600}
+              component="label"
+              htmlFor="password"
+              mb="5px"
+            >
+              Password
+            </Typography>
+            <CustomTextField
+              id="password"
+              type="password"
+              variant="outlined"
+              fullWidth
+              value={password}
+              onChange={(e: { target: { value: React.SetStateAction<string>; }; }) => setPassword(e.target.value)}
+              required
+            />
+          </Box>
+          {/* <Stack
+            justifyContent="space-between"
+            direction="row"
+            alignItems="center"
+            my={2}
+          >
+            <FormGroup>
+              <FormControlLabel
+                control={<Checkbox defaultChecked />}
+                label="Remember this Device"
+              />
+            </FormGroup>
+            <Typography
+              component={Link}
+              href="/"
+              fontWeight="500"
+              sx={{
+                textDecoration: "none",
+                color: "primary.main",
+              }}
+            >
+              Forgot Password ?
+            </Typography>
+          </Stack> */}
+        </Stack>
+        <Box sx={{ my: 2 }}>
+          <Button
+            sx={{ color: "white" }}
+            variant="contained"
+            size="large"
+            fullWidth
+            type="submit"
+          >
+            Sign In
+          </Button>
+        </Box>
+      </form>
+
+      {subtitle}
+    </>
+  );
+};
 
 export default AuthLogin;
