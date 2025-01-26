cf0c7b1a8cf83e0297a6ba43552245b10785bd32
Darfito <darfito.danurdoro-2021@fst.unair.ac.id>
2025-01-26 08:33:57 +0700

    fix: signout url

--- src/app/(DashboardLayout)/layout/sidebar/Logout.tsx
+++ src/app/(DashboardLayout)/layout/sidebar/Logout.tsx
@@ -1,3 +1,4 @@
+import { signout } from "@/app/authentication/signout/action";
 import { getLoggedInUser, getUserById } from "@/libs/sessions";
 import { Box, Typography, Avatar, IconButton, CircularProgress } from "@mui/material";
 import { IconLogout } from "@tabler/icons-react";
@@ -10,24 +11,11 @@ interface LogoutProps {
   loadingState: boolean
 }
 
-export const Logout = ({nama, role, loadingState} : LogoutProps) => {
-  const [loading, setLoading] = useState<boolean>(loadingState);  // Tambahkan loading state
-  const router = useRouter();
-
+export const Logout = ({nama, role} : LogoutProps) => {
 
   // Fungsi untuk menangani sign-out
   const handleLogout = async () => {
-    const response = await fetch('/api/authentication/signout', {
-      method: 'POST',
-    });
-
-    if (response.ok) {
-      // Jika sign out berhasil, arahkan ke halaman login
-      router.push('/authentication/login');
-    } else {
-      // Jika terjadi error, bisa ditangani di sini
-      console.error('Error signing out');
-    }
+    await signout();
   };
 
   return (
@@ -54,20 +42,15 @@ export const Logout = ({nama, role, loadingState} : LogoutProps) => {
         />
       </Box>
       <Box sx={{ width: "33%" }}>
-        {/* Menampilkan loading spinner saat data sedang dimuat */}
-        {/* {loading ? (
-          <CircularProgress />
-        ) : (
-          <> */}
             <Typography variant="h6">{nama || "Nama tidak tersedia"}</Typography>
             <Typography variant="subtitle1">{role || "Role tidak tersedia"}</Typography>
-          {/* </> */}
-        {/* )} */}
       </Box>
       <Box sx={{ width: "10%" }}>
-        <IconButton onClick={handleLogout}>
+        <form action={handleLogout}>
+        <IconButton type='submit'>
           <IconLogout />
         </IconButton>
+        </form>
       </Box>
     </Box>
   );
